0bdf89f409b385fc83d26a055218ae0977694cdd
T0LUL0PE <tolulopeadams7000@gmail.com>
2022-10-16 22:21:03 +0100

    Implement authentication functionality

--- lib/login.dart
+++ lib/login.dart
@@ -1,31 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
-  
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+
+}
+class _LoginFormState extends State<LoginForm>{
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    emailTextController.dispose();
+    passwordTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        TextFormField(
-          decoration: const InputDecoration(
-            icon: Icon(Icons.person),
-            hintText: 'Email',
-          )
-        ),
-        TextFormField(
+        children: [
+          TextFormField(
             decoration: const InputDecoration(
               icon: Icon(Icons.person),
+              hintText: 'Email',
+            ),
+            controller: emailTextController,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: Icon(Icons.lock),
               hintText: 'Password',
-            )
-        ),
-        TextButton(
-            onPressed: (){
-
-            },
-            child: Text("Login")
-        )
-      ]
+            ),
+            controller: passwordTextController,
+          ),
+          TextButton(
+              onPressed: (){
+                login(emailTextController.text, passwordTextController.text);
+              },
+              child: const Text("Login")
+          )
+        ]
     );
+  }
+
+  void login(String email, String password) {
+    try {
+      FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,31 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class SignUp extends StatelessWidget {
-  const SignUp({super.key});
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+
+}
+class _SignUpFormState extends State<SignUpForm>{
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    emailTextController.dispose();
+    passwordTextController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
         children: [
           TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.person),
-                hintText: 'Email',
-              )
+            decoration: const InputDecoration(
+              icon: Icon(Icons.person),
+              hintText: 'Email',
+            ),
+            controller: emailTextController,
           ),
           TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.person),
-                hintText: 'Password',
-              )
+            decoration: const InputDecoration(
+              icon: Icon(Icons.lock),
+              hintText: 'Password',
+            ),
+            controller: passwordTextController,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: Icon(Icons.lock),
+              hintText: 'Confirm Password',
+            ),
+            controller: passwordTextController,
           ),
           TextButton(
               onPressed: (){
-
+                login(emailTextController.text, passwordTextController.text);
               },
-              child: Text("Login")
+              child: const Text("Login")
           )
         ]
     );
+  }
+
+  void login(String email, String password) {
+    try {
+      FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
   }
 }
